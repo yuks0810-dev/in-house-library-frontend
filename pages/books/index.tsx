d5e94659ea059d376d88639192effb8bbbe9cc3b
PC732_nakata_yuki <>
2022-03-21 19:08:15 +0900

    initial commit

---
+++ pages/books/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Index Page</title>
+      </Head>
+      <main>
+        <h1>ここはトップページです</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./components/**/*.tsx", "./pages/**/*.tsx", "./public/**/*.html"],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
